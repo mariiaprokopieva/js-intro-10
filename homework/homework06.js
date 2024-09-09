6e350727cbb3b357de6292664395d57ad3db2e79
mariiaprokopieva <mariiaprokopieva@gmail.com>
2024-09-09 10:29:29 -0400

    updated homework06

--- homework/homework06.js
+++ homework/homework06.js
@@ -244,8 +244,8 @@ console.log('\n------------------TASK09------------------\n');
     
 const removeDuplicates = arr => {
     let result = [];
-    for (let i = 0; i < arr.length; i++) {
-        if(!result.includes(arr[i])) result.push(arr[i]);
+    for (const num of arr) {
+        if(!result.includes(num)) result.push(num);
     }
     return result;
 }
